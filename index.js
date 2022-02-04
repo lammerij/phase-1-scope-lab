51f02232b45c1a75177803b1239b6bfd1eac6a21
+ <+>
2022-02-03 20:56:58 -0500

    commit tree d8cb548720801153447272d913d82d1317ef0d9f

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = "BOB";
+}
+
+function setBestCustomer() {
+  console.log(setBestCustomer);
+  var bestCustomer = "not bob";
+  return setBestCustomer();
+}
